38e23a1419497dbd2facfb7a07b4086a30b04ded
flavioasp77 <flavioasp77@gmail.com>
2023-02-23 21:42:22 -0300

    Adicionadas algumas annotations na classe REcipe

--- src/main/java/com/exercice/recipeapp/model/Recipe.java
+++ src/main/java/com/exercice/recipeapp/model/Recipe.java
@@ -19,23 +19,33 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private long id;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Column(name="title", length = 100, nullable = false)
     private String title;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Column(name="description", length = 250, nullable = false)
     private String description;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    @Temporal(TemporalType.DATE)
     private Date creationDate;
+
     @Transient
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private User author;
+
     @Transient
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Ingredient> ingredients;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @Transient
     private Category category;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Column(name="methodOfPreparation", length = 3000, nullable = false)
     private String methodOfPreparation;
 
     public Recipe(long id, Date creationDate, String title) {
