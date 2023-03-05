e9f9060fc8bd998848e42cd0b78cd2ae22e31ebc
flavioasp77 <flavioasp77@gmail.com>
2023-03-05 09:00:44 -0300

    Criado os endpoints find e delete receitas

--- src/main/java/com/exercice/recipeapp/controller/RecipeController.java
+++ src/main/java/com/exercice/recipeapp/controller/RecipeController.java
@@ -3,10 +3,13 @@ package com.exercice.recipeapp.controller;
 import com.exercice.recipeapp.model.Recipe;
 import com.exercice.recipeapp.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/recipeApp")
@@ -30,5 +33,24 @@ public class RecipeController {
     public Recipe updateRecipe(@PathVariable("id") Long id, @RequestBody Recipe recipe) {
         recipe.setId(id);
         return recipeRepository.save(recipe);
+    }
+
+    @GetMapping(value = "/findRecipe/{id}")
+    public ResponseEntity<?> findRecipe(@PathVariable("id") long id) {
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        if (recipe.isPresent()) {
+            return ResponseEntity.ok().body(recipe);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Recipe with id: %s not found.", id));
+    }
+
+    @DeleteMapping(value = "/deleteRecipe/{id}")
+    public ResponseEntity<?> deleteRecipe(@PathVariable("id") long id) {
+        try {
+            recipeRepository.deleteById(id);
+            return ResponseEntity.ok().body("Recipe deleted successfully!");
+        } catch (Exception error) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Recipe not found for deletion!");
+        }
     }
 }
